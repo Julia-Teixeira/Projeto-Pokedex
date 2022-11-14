4d0756ef30658ef93383f66778b91b0061450615
Julia Pereira <juliapt95@gmail.com>
2022-11-14 00:27:26 -0300

    v-01

--- script/index.js
+++ script/index.js
@@ -1,20 +1,32 @@
-import { getPokemons, getIdPokemon} from '../script/requestAPI.js';
+import { getPokemons, getPokemon, getTextPokemon} from '../script/requestAPI.js';
+import { openModal } from './modalPokemon.js';
+
 let lista = JSON.parse(localStorage.getItem('Lista'))
+const btnCarregar = document.querySelector('.btnCarregar');
 
 const ulListaPokemon = document.querySelector('.lista-pokemons');
 
-function montarCards(id, nome, types, img) {
+ function montarCards(id, nome, types, img) {
+  let idTratado
+  if(id < 10){
+    idTratado = `#00${id}`
+  } else if(id >= 10 && id < 100){
+    idTratado = `#0${id}`
+  } else {
+    idTratado = `#${id}`
+  }
  
   ulListaPokemon.insertAdjacentHTML('beforeend',
     `
-    <li class="pokemon " id="${nome}">
-      <div class="type-${types[0].type.name}-border-color div-up">
-        <p class="text-regular text-regular-8">#${id}</p>
+    <li class="pokemon type-${types[0].type.name}-border-color" id="${nome}">
+      <div class="div-up">
+        <p class="text-regular text-regular-8">${idTratado}</p>
         <img src="${img}" alt="${nome}">
-      </div>
-      <div class="type-${types[0].type.name}-background div-down">
+        <div class="type-${types[0].type.name}-background div-down">
         <p class="text-regular text-regular-10">${nome}</p>
       </div>
+      </div>
+
     </li>
   `
   )
@@ -24,9 +36,9 @@ function montarCards(id, nome, types, img) {
 function searchPokemon(){
 
   const inpSearch = document.querySelector('.search')
-  inpSearch.addEventListener('keyup', () => {
+  inpSearch.addEventListener('keyup', async () => {
     ulListaPokemon.innerHTML= ""
-
+   
     lista.forEach((e) => {
       if(e.nome.includes((inpSearch.value).toLowerCase())){
         montarCards(e.id, e.nome, e.types, e.img )
@@ -40,9 +52,11 @@ function searchPokemon(){
     
   })
 }
+
 function filterPokemon(){
   const filter = document.querySelector('.filter')
   filter.addEventListener('click', () =>{
+    let lista = JSON.parse(localStorage.getItem('Lista'))
     ulListaPokemon.innerHTML= ""
     filter.classList.toggle('filterName')
   
@@ -60,21 +74,48 @@ function filterPokemon(){
           }
         })
       })
-    } else {
-      lista.forEach((e) => {
-        montarCards(e.id, e.nome, e.types, e.img )
+    } else if(filter.classList.contains('filterNumber')){
+      let idPokemon = []
+      lista.forEach(({id}) => {
+        idPokemon.push(id)
+      })
+      
+      let idPokemonOrdenada = idPokemon.sort((a,b) => {
+        return a - b
+      })
+
+      idPokemonOrdenada.forEach(e => {
+        lista.forEach((pokemon) => {
+          if (e == pokemon.id) { 
+            montarCards(pokemon.id, pokemon.nome, pokemon.types, pokemon.img )
+          }
+        })
       })
     }
   
   })
 }
 
+btnCarregar.addEventListener('click', () => {
+  
+      let currentOffset = JSON.parse(localStorage.getItem('currentOffset'))
+      getPokemons(currentOffset + 21)
+})
 
+function cardPokemonDetails(){
+  ulListaPokemon.addEventListener('click', async (event) => {
+    
+    if(event.path[2].classList.contains('pokemon')){
+      openModal(event.path[2].id)
+      const text = await getTextPokemon(event.path[2].id)  
+    }
+    
+  })
+}
 
 filterPokemon()
 searchPokemon()
-
+cardPokemonDetails()
 export {
   montarCards
 }
-  // https://cdn.traction.one/pokedex/pokemon/
--- script/requestAPI.js
+++ script/requestAPI.js
@@ -1,16 +1,19 @@
 import { montarCards } from "../script/index.js";
 const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
-let listPokemon = [];
+let listPokemon = []
+let offset = 0
 
 
-const getPokemons = async () => {
-  const response = await fetch(baseURL + '?limit=56', {
+const getPokemons = async (currentOffset) => {
+  
+  const response = await fetch(`${baseURL}?offset=${currentOffset}&limit=21`, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json'
     }
   });
 
+  localStorage.setItem(`currentOffset`, currentOffset)
   localStorage.setItem('Lista', '')
   const result = await response.json();
   const pokemons = [...result.results]
@@ -18,7 +21,7 @@ const getPokemons = async () => {
     getIdPokemon(e.name) 
   })
 }
-
+getPokemons(offset)
 const getIdPokemon =  (nome) => {
     try {
       fetch(`${baseURL}${nome}`).then(request => { return request.json() }).then(response => {
@@ -30,13 +33,37 @@ const getIdPokemon =  (nome) => {
         listPokemon.push(poke)
 
         localStorage.setItem('Lista', JSON.stringify(listPokemon))
+        return response
       })
-     
+
     } catch (error) {
       return error
     }
 }
+const getPokemon = async (name) => {
+  
+  const request = await fetch(`${baseURL}${name}`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const response = await request.json()
+  return response
+}
 
-getPokemons()
+const getTextPokemon = async (name) => {
 
-export {getPokemons, listPokemon, getIdPokemon}
+  const request = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const response = await request.json()
+  
+  return response.flavor_text_entries[9].flavor_text
+  
+}
+
+export {getPokemons, listPokemon, getIdPokemon, offset, getPokemon, getTextPokemon}
